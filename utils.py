21739b57cc93c8945162ce6de001b7ae84e19c48
LeahDZuckerman <LeahDZuckerman@gmail.com>
2023-07-19 17:26:40 -0600

    more work on NN

--- utils.py
+++ utils.py
@@ -137,6 +137,9 @@ def collect_curves_tofiles(n_curves, n_timesteps=1000, downsize_method='interpol
     """
     Add raw light curves (row-wise) into csv files storing flux, time, and labels (1 = transit, 0 = no transit).
     Every call to this function will add rows to these csv files. 
+    NOTE: this should save the file if the error is lightcurve failing to download the next star, 
+          but if something happens (e.g. user interupts) during file opening, I think sometimes the 
+          files done't get saved correctly. 
     ----------
     Parameters:
         n_curves: number of curves to download (will be approximate if percentages don't work out)
@@ -169,7 +172,7 @@ def collect_curves_tofiles(n_curves, n_timesteps=1000, downsize_method='interpol
     # Create files if they don't exist, else check that they have the same length
     filepaths = []
     filelengths = []
-    for tag in ['flux', 'time', 'label']:
+    for tag in ['flux', 'time', 'labels']:
         filepath = f"{savepath}/{tag}_all_{n_timesteps}_{pct_transit}.csv"
         filepaths.append(filepath)
         if os.path.exists(filepath) == False:
@@ -177,11 +180,11 @@ def collect_curves_tofiles(n_curves, n_timesteps=1000, downsize_method='interpol
             open(filepath, 'w')
             filelengths.append([0])
         else:
-            print(f'Adding {tag} rows to {filepath}')
+            print(f'Adding {tag} to {filepath}')
             filelengths.append(len(pd.read_csv(filepath, header=None, delimiter=',')))
     if all(element == filelengths[0] for element in filelengths) == False:
-        raise Exception(f'{filepaths[0]}, {filepaths[1]}, and {filepaths[2]}, do not all have the same number of rows.')
-        
+        raise Exception(f'{filepaths[0]}, {filepaths[1]}, and {filepaths[2]}, have different number of rows ({filelengths[0]}, {filelengths[1]}, and {filelengths[2]}).')
+
     # Download curves and append to files
     with open(rf'{filepaths[0]}','a') as f1, open(rf'{filepaths[1]}','a') as f2, open(rf'{filepaths[2]}','a') as f3:
         writer1=csv.writer(f1)
@@ -207,14 +210,14 @@ def collect_curves_tofiles(n_curves, n_timesteps=1000, downsize_method='interpol
                 flux = flux[0:n_timesteps]
                 time = time[0:n_timesteps]
             # Get label
-            label = 1 if star in transit_ids else 0
+            label = 1 if (star in transit_ids) else 0
             # Add to csv files
             writer1.writerow(np.array(flux))
             writer2.writerow(np.array(time))
             writer3.writerow(np.array([label]))
             i += 1
 
-#### Functions for use with NNs (for Leah) ####
+#### Functions for use with NNs ####
 import torch
 from torch import nn
 from sklearn.datasets import make_classification
@@ -229,9 +232,9 @@ def check_inputs(train_ds, train_loader):
     print(f'     {len(train_ds)} obs, broken into {len(train_loader)} batches')
     train_features, train_labels = next(iter(train_loader))
     shape = train_features.size()
-    print(f'     Each batch has data of shape {train_features.size()}, e.g. {shape[0]} obs, {shape[1]} features') #{[shape[2], shape[3]]} pixels each, {shape[1]} layers (features)')
+    print(f'     Each batch has data of shape {train_features.size()}, e.g. {shape[0]} obs, {shape[2]} pixels, {shape[1]} feature channels') #{[shape[2], shape[3]]} pixels each, {shape[1]} layers (features)')
     shape = train_labels.size()
-    print(f'     Each batch has labels of shape {train_labels.size()}')#, e.g. {shape[0]} obs, {shape[1]} classes') #{[shape[2], shape[3]]} pixels each, {shape[1]} layers (classes)')
+    print(f'     Each batch has labels of shape {train_labels.size()}, e.g. {shape[0]} obs, {shape[1]} classes') #{[shape[2], shape[3]]} pixels each, {shape[1]} layers (classes)')
 
 
 class MyDataset(Dataset):
@@ -240,39 +243,137 @@ class MyDataset(Dataset):
   '''
 
   def __init__(self, X_train, y_train, norm=True, impute_nans=True):
-    self.X = torch.from_numpy(X_train.astype(np.float32))
-    self.y = np.squeeze(torch.from_numpy(y_train).type(torch.LongTensor))
-    self.len = self.X.shape[0]
+    self.x = torch.from_numpy(X_train.astype(np.float32))
+    self.y = np.squeeze(torch.from_numpy(y_train))#.type(torch.LongTensor))
+    self.len = self.x.shape[0]
     self.norm = norm
     self.impute_nans = impute_nans
   
   def __getitem__(self, index):
-    X = self.X[index]
+    # Get lightcurve
+    x = self.x[index]
     if self.norm: 
-      X = ((X - np.nanmin(X))/(np.nanmax(X) - np.nanmin(X))) # min-max normalization to [0, 1] 
+      x = ((x - np.nanmin(x))/(np.nanmax(x) - np.nanmin(x))) # min-max normalization to [0, 1] 
     if self.impute_nans: 
-      X_before = X.clone().detach()
-      X = torch.tensor(pd.Series(X).interpolate(limit_direction='both')) # fill NaNs with means of nieghboring values
+      x = torch.tensor(pd.Series(x).interpolate(limit_direction='both')) # fill NaNs with means of nieghboring values
+    X = np.zeros((1, len(x)), dtype=np.float32) # Add dummy axis since only one channel
+    X[0, :] = x 
+    # Get label
     y = self.y[index]
-
-    return X, y
+    mask = np.zeros(2, dtype=np.float32)
+    mask[int(y)] = 1 # e.g. if y=0, first idx is 1, if y=1, second idx is 1
+    return X, mask
     
   def __len__(self):
     return self.len
 
 
+def validate(val_loader, model):
+    '''
+    Function to calculate validation accuracy
+    '''
+    num_correct = 0
+    num_pixels = 0
+    dice_score = 0
+    model.eval() # set model into eval mode
+    with torch.no_grad():
+        for x, y in val_loader:
+            x = x.to("cpu")
+            y = y.to("cpu")#.unsqueeze(1)
+            probs = torch.sigmoid(model(x)) # call model to get output, apply sigmoid to get class probs
+            preds = (probs > 0.5).float() # turn regression value (between zero and 1, e.g. "prob of being 1") into predicted 0s and 1s
+            num_correct += len(np.where(preds == y)[0]) #(preds == y).sum()
+            num_pixels += len(preds.flatten()) # torch.numel(preds)
+            dice_score += (2 * (preds * y).sum()) / ((preds + y).sum() + 1e-8)
+    accuracy = num_correct/num_pixels*100
+
+    return accuracy, dice_score
+
+
 class SimpleNN(nn.Module):
   '''
-  Simple two-layer feed-foward NN
-  Oddly, this works very well. 
+  Simple feed-foward NN
+    - does not work well at all
+    - get rid of extra channels dim in last layer (channels not used in linear, but extra dim for completness)
   '''
 
-  def __init__(self, input_dim, output_dim, hidden_layers): 
+  def __init__(self, input_dim, output_dim, n_feats): 
     super(SimpleNN, self).__init__()
-    self.linear1 = nn.Linear(input_dim, hidden_layers) 
-    self.linear2 = nn.Linear(hidden_layers, output_dim) # so becasue output_dim is num classes, outputs will be class probs?
+    self.layers = nn.ModuleList()
+    n_feats = [input_dim] + n_feats + [output_dim]
+    for i in range(len(n_feats)-1):
+       self.layers.append(nn.Linear(n_feats[i], n_feats[i+1]))
     
   def forward(self, x):
-    x = torch.sigmoid(self.linear1(x))
-    x = self.linear2(x)
+    for i in range(len(self.layers)):
+        x = self.layers[i](x) # torch.sigmoid(self.layers[i](x))
+    x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
     return x
+
+
+class ConvNN1(nn.Module):
+  '''
+  1D convolutional NN
+    - based on https://www.kaggle.com/code/purplejester/pytorch-deep-time-series-classification
+  '''
+
+  def __init__(self, in_channels, out_channels, conv_channels=[64, 128, 256], k_size=3): 
+    super(ConvNN1, self).__init__()
+
+    self.convs = nn.Sequential(
+        nn.Conv1d(in_channels, conv_channels[0], kernel_size=k_size, padding='same'),
+        nn.Conv1d(conv_channels[0], conv_channels[1], kernel_size=k_size, padding='same'),
+        nn.Conv1d(conv_channels[1], conv_channels[2], kernel_size=k_size, padding='same'))
+    
+    self.linears = nn.Sequential(
+        nn.Dropout(p=0.5),
+        nn.Linear(256, 64),
+        nn.ReLU(inplace=True),
+        nn.Dropout(p=0.5),
+        nn.Linear(64, 64), 
+        nn.ReLU(inplace=True),
+        nn.Linear(64, 64))
+
+  def forward(self, x):
+    print(f'Before convs {x.shape}')
+    x = self.convs(x) 
+    print(f'After convs {x.shape}')
+    x = x.view(x.size(0), -1)
+    print(f'After reshape {x.shape}')
+    x = self.linears(x) 
+    return x
+
+class ConvNN2(nn.Module):
+  '''
+  1D convolutional NN
+  - Based on https://jovian.com/ningboming/time-series-classification-cnn
+  '''
+
+  def __init__(self, in_channels, out_channels, conv_channels=[64, 128, 256], k_size=3): 
+    super(ConvNN2, self).__init__()
+
+    self.convs = nn.Sequential(
+        nn.Conv1d(in_channels, conv_channels[0], kernel_size=k_size, stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv1d(conv_channels[0], conv_channels[1], kernel_size=k_size, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool1d(2), 
+        nn.Conv1d(conv_channels[1], conv_channels[2], kernel_size=k_size, stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv1d(conv_channels[2], conv_channels[2], kernel_size=k_size, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool1d(2)) 
+
+    self.linears = nn.Sequential(
+        nn.Flatten(), 
+        nn.Linear(256*31, 1024),
+        nn.ReLU(),
+        nn.Linear(1024, 512),
+        nn.ReLU(),
+        nn.Linear(512, out_channels))
+
+  def forward(self, x):
+    x = self.convs(x) 
+    x = self.linears(x) 
+    return x  
+
