f3bb7b1cc78a950493d36ed437e4225705e88fcf
Ashutosh Gandhi <ashyo98@outlook.com>
2023-07-21 13:42:55 -0600

    Adds req.txt and data-download starter script making use to utils function

---
+++ download_data.py
@@ -0,0 +1,7 @@
+from utils import collect_curves_tofiles
+
+if __name__ == "__main__":
+    n_curves = 200
+    pct_transit = 50
+    savepath = 'Data'
+    collect_curves_tofiles(n_curves=n_curves, pct_transit=pct_transit, savepath=savepath)
